075dd8571d609df6cf5fdf4ae503590413bda73d
lazean214 <salazar.neil@gmail.com>
2026-06-16 17:09:43 +0400

    fix mail service

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,33 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
+use Mailtrap\Helper\ResponseHelper;
+use Mailtrap\MailtrapClient;
+use Mailtrap\Mime\MailtrapEmail;
+use Symfony\Component\Mime\Address;
 use Illuminate\Support\Facades\Schedule;
+
+Artisan::command('send-mail', function () {
+    $email = (new MailtrapEmail())
+        ->from(new Address('noreply@naicatech.com', 'The CRM'))
+        ->to(new Address('ncs.it02@outlook.com'))
+        ->subject('You are awesome!')
+        ->text('Congrats for sending test email with Mailtrap!')
+    ;
+
+    $response = MailtrapClient::initSendingEmails(
+        apiKey: '76226dca9a847a0b796ee1e1868948b1'
+    )->send($email);
+
+    var_dump(ResponseHelper::toArray($response));
+})->purpose('Send Mail via API');
+
+Artisan::command('test-mail', function () {
+    \Illuminate\Support\Facades\Mail::raw('Test email from The CRM via SMTP', function ($m) {
+        $m->to('ncs.it02@outlook.com')->subject('SMTP Test');
+    });
+    $this->info('Email sent!');
+})->purpose('Test SMTP mail');
 
 Schedule::command('queue:work --stop-when-empty')
     ->everyMinute()
